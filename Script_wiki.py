05354374b6304b3070ccc00cc73e14512a2de096
Victor Bourman <Victorbourman77@gmail.com>
2023-05-03 14:17:15 +0300

    Update Script_wiki.py

--- Script_wiki.py
+++ Script_wiki.py
@@ -2,17 +2,17 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
-
+# take the info form wiki
 url = "https://en.wikipedia.org/wiki/List_of_animal_names"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
-
+#selects all the rows in the table
 table = soup.find("table", {"class": "wikitable sortable"})
 rows = table.find_all("tr")[1:]
-
+#Open desktop and create there file name "animal_names".
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 filename = os.path.join(desktop_path, "animal_names.txt")
-
+#take all the input and put on the file "animal_names"
 with open(filename, "w") as file:
     for row in rows:
         cols = row.find_all("td")
